5ba856ef16d38c07faae6f46d864439272b1a0ee
pikaybh <pikabh@naver.com>
2022-12-09 14:23:02 +0900

    202212091422

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          정적 사이트 업데이트 하기 귀찮아서 그냥 다 부서버림 ㅅㄱ
+          정적 사이트 업데이트 하기 귀찮아서 그냥 다 부서버림 ㅅㄱ<br/>
+          🔨🔨🔨🔨🔨
+        </p>
+        <p>
+          Jekyll 잘가👋<br/>
+          넌 나랑 안 맞는거 같아 ㅠㅠ<br/>
+          나 사실 React 좋아해...
         </p>
       </header>
     </div>
