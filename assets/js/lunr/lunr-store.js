00672fffd0a07346de67c609c82d80f0ce491323
pikaybh <pikaybh@users.noreply.github.com>
2024-07-02 12:59:45 +0000

    jekyll build from Action fa63a2c37dcd1182a0cbd5b838fabdc5c35fcdfa

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,121 @@
+var store = [{
+        "title": "Automating Construction Safety Management Documentation through Integration of Retrieval Augmented Generation with Large Language Models",
+        "excerpt":"by Byunghee Yoo1, Sungwon Ahn2, and Changbum R. Ahn3, * 1 Department of Architecture and Architectural Engineering, Seoul National University, Gwanak-gu, Seoul 08826, Republic of Korea 2 Department of Architecture and Architectural Engineering, Seoul National University, Gwanak-gu, Seoul 08826, Republic of Korea 3 Department of Architecture and Architectural Engineering, Seoul...","categories": [],
+        "tags": [],
+        "url": "/conferences/2024-05-08/",
+        "teaser": null
+      },{
+        "title": "Baz Boom Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/baz-boom-identity/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "Fizz Bang Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/fizz-bang-identity/",
+        "teaser": "/assets/images/unsplash-gallery-image-2-th.jpg"
+      },{
+        "title": "Foo Bar Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/foo-bar-website/",
+        "teaser": "/assets/images/foo-bar-identity-th.jpg"
+      },{
+        "title": "Ginger Gulp Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/ginger-gulp-identity/",
+        "teaser": "/assets/images/unsplash-gallery-image-3-th.jpg"
+      },{
+        "title": "KOSAS 2024 Spring Jeju Conference",
+        "excerpt":"May 8th ~ May 10th  ","categories": [],
+        "tags": [],
+        "url": "/2024-KOSAS-conference/",
+        "teaser": null
+      },{
+        "title": "Harnessing Generative Pre-Trained Transformers for Construction Accident Prediction with Saliency Visualization",
+        "excerpt":"by Byunghee Yoo1, Jinwoo Kim2, Seongeun Park1, Changbum R. Ahn1, *, and Taekeun Oh3, * 1 Department of Architecture and Architectural Engineering, Seoul National University, Gwanak-gu, Seoul 08826, Republic of Korea 2 Department of Architectural Engineering, Gachon University, Seongnam-si 13120, Gyeonggi-do, Republic of Korea 3 Department of Safety Engineering, Incheon...","categories": [],
+        "tags": [],
+        "url": "/publications/2024-01-12/",
+        "teaser": null
+      },,{
+    "title": "Harnessing Generative Pre-Trained Transformers for Construction Accident Prediction with Saliency Visualization",
+    "excerpt":"by Byunghee Yoo1, Jinwoo Kim2, Seongeun Park1, Changbum R. Ahn1, *, and Taekeun Oh3, * 1 Department of Architecture and Architectural Engineering, Seoul National University, Gwanak-gu, Seoul 08826, Republic of Korea 2 Department of Architectural Engineering, Gachon University, Seongnam-si 13120, Gyeonggi-do, Republic of Korea 3 Department of Safety Engineering, Incheon...","url": "https://pikaybh.github.io/_publications/2024-01-12/"
+  },{
+    "title": "Hello, World!",
+    "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","url": "https://pikaybh.github.io/_drafts/2024-04-05-hello-world-welcome/"
+  },{
+    "title": "KOSAS 2024 Spring Jeju Conference",
+    "excerpt":"May 8th ~ May 10th  ","url": "https://pikaybh.github.io/_posts/2024-05-08-2024-KOSAS-conference/"
+  },{
+    "title": "Automating Construction Safety Management Documentation through Integration of Retrieval Augmented Generation with Large Language Models",
+    "excerpt":"by Byunghee Yoo1, Sungwon Ahn2, and Changbum R. Ahn3, * 1 Department of Architecture and Architectural Engineering, Seoul National University, Gwanak-gu, Seoul 08826, Republic of Korea 2 Department of Architecture and Architectural Engineering, Seoul National University, Gwanak-gu, Seoul 08826, Republic of Korea 3 Department of Architecture and Architectural Engineering, Seoul...","url": "https://pikaybh.github.io/_conferences/2024-05-08/"
+  },{
+    "title": "Page Not Found",
+    "excerpt":"Sorry, but the page you were trying to view does not exist.    ","url": "https://pikaybh.github.io/404.html"
+  },{
+    "title": "About Me",
+    "excerpt":"I am a Master’s degree student at Seoul National University. I received my B.S. degree from Dankook University in 2023, respectively. I am currently located in Seoul, Korea, and work as a researcher in Seoul National University Construction Engineering &amp; Management Lab. (SNUCEM). My research interests include Construction Safety and...","url": "https://pikaybh.github.io/about/"
+  },{
+    "title": "Archive Layout with Content",
+    "excerpt":"A variety of common markup showing how the theme styles them. Header one Header two Header three Header four Header five Header six Blockquotes Single line blockquote: Stay hungry. Stay foolish. Multi line blockquote with a cite reference: People think focus means saying yes to the thing you’ve got to...","url": "https://pikaybh.github.io/archive-layout-with-content/"
+  },{
+    "title": "Baz Boom Identity",
+    "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","url": "https://pikaybh.github.io/_portfolio/baz-boom-identity/"
+  },{
+    "title": "Posts by Category",
+    "excerpt":" ","url": "https://pikaybh.github.io/categories/"
+  },{
+    "title": "Posts by Collection",
+    "excerpt":"conferences Automating Construction Safety Management Documentation through Integration of Retrieval Augmented Generation with Large Language Models 1 minute read Byunghee Yoo, Sungwon Ahn, &amp; Changbum R. Ahn (May 9th, 2024) docs pets portfolio Baz Boom Identity Baz Boom design system including logo mark, website design, and branding applications. Fizz Bang...","url": "https://pikaybh.github.io/collection-archive/"
+  },{
+    "title": "Fizz Bang Identity",
+    "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","url": "https://pikaybh.github.io/_portfolio/fizz-bang-identity/"
+  },{
+    "title": "Foo Bar Identity",
+    "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","url": "https://pikaybh.github.io/_portfolio/foo-bar-website/"
+  },{
+    "title": "Ginger Gulp Identity",
+    "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","url": "https://pikaybh.github.io/_portfolio/ginger-gulp-identity/"
+  },{
+    "title": null,
+    "excerpt":" ","url": "https://pikaybh.github.io/home/"
+  },{
+    "title": "Byunghee Yoo",
+    "excerpt":"    ","url": "https://pikaybh.github.io/"
+  },{
+    "title": null,
+    "excerpt":"var idx = lunr(function () { this.field('title') this.field('excerpt') this.field('categories') this.field('tags') this.ref('id') this.pipeline.remove(lunr.trimmer) for (var item in store) { this.add({ title: store[item].title, excerpt: store[item].excerpt, categories: store[item].categories, tags: store[item].tags, id: item }) } }); $(document).ready(function() { $('input#search').on('keyup', function () { var resultdiv = $('#results'); var query = $(this).val().toLowerCase(); var result = idx.query(function...","url": "https://pikaybh.github.io/assets/js/lunr/lunr-en.js"
+  },{
+    "title": null,
+    "excerpt":"step1list = new Array(); step1list[\"ΦΑΓΙΑ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΟΥ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΩΝ\"] = \"ΦΑ\"; step1list[\"ΣΚΑΓΙΑ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΟΥ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΩΝ\"] = \"ΣΚΑ\"; step1list[\"ΟΛΟΓΙΟΥ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΑ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΩΝ\"] = \"ΟΛΟ\"; step1list[\"ΣΟΓΙΟΥ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΑ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΩΝ\"] = \"ΣΟ\"; step1list[\"ΤΑΤΟΓΙΑ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΟΥ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΩΝ\"] = \"ΤΑΤΟ\"; step1list[\"ΚΡΕΑΣ\"]...","url": "https://pikaybh.github.io/assets/js/lunr/lunr-gr.js"
+  },{
+    "title": null,
+    "excerpt":"var store = [ {%- for c in site.collections -%} {%- if forloop.last -%} {%- assign l = true -%} {%- endif -%} {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%} {%- for doc in docs -%} {%- if doc.header.teaser -%} {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture...","url": "https://pikaybh.github.io/assets/js/lunr/lunr-store.js"
+  },{
+    "title": "Page Archive",
+    "excerpt":"{% for post in site.pages %}   {% unless post.hidden %}     {% include archive-single.html %}   {% endunless %} {% endfor %} ","url": "https://pikaybh.github.io/page-archive/"
+  },{
+    "title": "Portfolio",
+    "excerpt":"Sample document listing for the collection `_portfolio`.     {% for post in site.portfolio %}     {% include archive-single.html type=\"grid\" %}   {% endfor %} ","url": "https://pikaybh.github.io/portfolio/"
+  },{
+    "title": "Post Archive with Feature Rows",
+    "excerpt":"{% for post in site.posts limit: 5 %}   {% include archive-single.html %} {% endfor %}  {% include feature_row id=\"intro\" type=\"center\" %}  {% include feature_row %}  {% include feature_row id=\"feature_row2\" type=\"left\" %}  {% include feature_row id=\"feature_row3\" type=\"right\" %}  {% include feature_row id=\"feature_row4\" type=\"center\" %}","url": "https://pikaybh.github.io/post-archive-feature-rows/"
+  },{
+    "title": "Publications",
+    "excerpt":"{% include base_path %} International Journal Papers {% capture written_year %}'None'{% endcapture %} {% for post in site.publications %} {% capture year %}{{ post.date | date: '%Y' }}{% endcapture %} {% if year != written_year %} {{ year }} {% capture written_year %}{{ year }}{% endcapture %} Harnessing Generative Pre-Trained...","url": "https://pikaybh.github.io/publications/"
+  },{
+    "title": "Sitemap",
+    "excerpt":"A list of all the posts and pages found on the site. For you robots out there is an [XML version]({{ \"sitemap.xml\" | relative_url }}) available for digesting as well. Pages {% for post in site.pages %} {% include archive-single.html %} {% endfor %} Posts {% for post in site.posts...","url": "https://pikaybh.github.io/sitemap/"
+  },{
+    "title": "Posts by Year",
+    "excerpt":"{% include base_path %} {% capture written_year %}'None'{% endcapture %} {% for post in site.posts %} {% capture year %}{{ post.date | date: '%Y' }}{% endcapture %} {% if year != written_year %} {{ year }} {% capture written_year %}{{ year }}{% endcapture %} {% endif %} {% include archive-single.html...","url": "https://pikaybh.github.io/year-archive/"
+  },{
+    "title": null,
+    "excerpt":"{% if page.xsl %} {% endif %} {% assign collections = site.collections | where_exp:'collection','collection.output != false' %}{% for collection in collections %}{% assign docs = collection.docs | where_exp:'doc','doc.sitemap != false' %}{% for doc in docs %} {{ doc.url | replace:'/index.html','/' | absolute_url | xml_escape }} {% if doc.last_modified_at or doc.date...","url": "https://pikaybh.github.io/sitemap.xml"
+  },{
+    "title": null,
+    "excerpt":"{% if page.xsl %}{% endif %}Jekyll{{ site.time | date_to_xmlschema }}{{ page.url | absolute_url | xml_escape }}{% assign title = site.title | default: site.name %}{% if page.collection != \"posts\" %}{% assign collection = page.collection | capitalize %}{% assign title = title | append: \" | \" | append: collection %}{% endif...","url": "https://pikaybh.github.io/feed.xml"
+  }]
