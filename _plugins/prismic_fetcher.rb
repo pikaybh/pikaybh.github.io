f573378607710d332ad1abfd5ba9d22930445132
B.H Yoo <pikabh@naver.com>
2025-01-27 18:13:39 +0900

    202501271813

--- _plugins/prismic_fetcher.rb
+++ _plugins/prismic_fetcher.rb
@@ -5,13 +5,18 @@ module Jekyll
     safe true
 
     def generate(site)
-      api_url = 'https://pikaybh-github-io.prismic.io/api/v2'
+      api_url = 'https://your-repo-name.prismic.io/api/v2'
       api = Prismic.api(api_url)
       response = api.query(Prismic::Predicates.at('document.type', 'posts'))
 
+      unless response.results.any?
+        puts "Warning: No documents found in Prismic for 'posts' type."
+        return
+      end
+
       response.results.each do |doc|
         if doc['posts'].nil?
-          puts "Error: Missing 'posts' field in Prismic response"
+          puts "Error: Missing 'posts' field in Prismic response for document ID #{doc.id}"
           next
         end
 
@@ -22,6 +27,8 @@ module Jekyll
           'date' => doc.first_publication_date || 'Unknown'
         }
       end
+    rescue StandardError => e
+      puts "Prismic API fetch error: #{e.message}"
     end
   end
 end
