b240507594f16a57f79d0399cd9e19cca605af5d
B.H Yoo <pikabh@naver.com>
2025-01-27 17:58:25 +0900

    202501271758

--- _plugins/prismic_fetcher.rb
+++ _plugins/prismic_fetcher.rb
@@ -1,21 +1,27 @@
 require 'prismic'
 
 module Jekyll
-    class PrismicGenerator < Generator
-        safe true
+  class PrismicGenerator < Generator
+    safe true
 
-        def generate(site)
-            api_url = 'https://pikaybh-github-io.cdn.prismic.io/api/v2'
-            api = Prismic.api(api_url)
-            response = api.query(Prismic::Predicates.at('document.type', 'posts'))
+    def generate(site)
+      api_url = 'https://pikaybh-github-io.prismic.io/api/v2'
+      api = Prismic.api(api_url)
+      response = api.query(Prismic::Predicates.at('document.type', 'posts'))
 
-            site.data['prismic_posts'] = response.results.map do |doc|
-                {
-                    'title' => doc['data.title'].as_text,
-                    'content' => doc['data.body'].as_html,
-                    'date' => doc.first_publication_date
-                }
-            end
+      response.results.each do |doc|
+        if doc['posts'].nil?
+          puts "Error: Missing 'posts' field in Prismic response"
+          next
         end
+
+        site.data['prismic_posts'] ||= []
+        site.data['prismic_posts'] << {
+          'title' => doc['posts.title']&.as_text || 'Untitled',
+          'content' => doc['posts.body']&.as_html || 'No content available',
+          'date' => doc.first_publication_date || 'Unknown'
+        }
+      end
     end
+  end
 end
