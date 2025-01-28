dc5109848b389e1d2e6a7994ab132b00693ffabc
B.H Yoo <pikabh@naver.com>
2025-01-28 22:34:21 +0900

    Notion Init

---
+++ _scripts/notion-webhook.js
@@ -0,0 +1,186 @@
+const { Client } = require("@notionhq/client");
+const { NotionToMarkdown } = require("notion-to-md");
+const moment = require("moment");
+const path = require("path");
+const fs = require("fs");
+const axios = require("axios");
+// or
+// import {NotionToMarkdown} from "notion-to-md";
+
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+});
+
+function escapeCodeBlock(body) {
+    const regex = /```([\s\S]*?)```/g;
+    return body.replace(regex, function (match, htmlBlock) {
+        return "\n{% raw %}\n```" + htmlBlock.trim() + "\n```\n{% endraw %}\n";
+    });
+}
+
+function replaceTitleOutsideRawBlocks(body) {
+    const rawBlocks = [];
+    const placeholder = "%%RAW_BLOCK%%";
+    body = body.replace(/{% raw %}[\s\S]*?{% endraw %}/g, (match) => {
+        rawBlocks.push(match);
+        return placeholder;
+    });
+
+    const regex = /\n#[^\n]+\n/g;
+    body = body.replace(regex, function (match) {
+        return "\n" + match.replace("\n#", "\n##");
+    });
+
+    rawBlocks.forEach(block => {
+        body = body.replace(placeholder, block);
+    });
+
+    return body;
+}
+
+// passing notion client to the option
+const n2m = new NotionToMarkdown({ notionClient: notion });
+
+(async () => {
+    // Call DB
+    const databaseId = process.env.BLOG_DB_ID;
+    let response = await notion.databases.query({
+        database_id: databaseId,
+        filter: {
+            property: "Publish",
+            checkbox: {
+                equals: true,
+            },
+        },
+    });
+
+    // Fetch into pages
+    const pages = response.results;
+    while (response.has_more) {
+        const nextCursor = response.next_cursor;
+        response = await notion.databases.query({
+            database_id: databaseId,
+            start_cursor: nextCursor,
+            filter: {
+                property: "Publish",
+                checkbox: {
+                    equals: true,
+                },
+            },
+        });
+        pages.push(...response.results);
+    }
+
+    for (const r of pages) {
+        const id = r.id;
+
+        // ensure directory exists
+        let post_type = r.properties?.["Type"]?.["select"];
+        let root = "_" + post_type?.["name"];
+        if(!fs.existsSync(root)) {
+            fs.mkdirSync(root, { recursive: true })
+        }
+
+        // date
+        let date = moment(r.created_time).format("YYYY-MM-DD");
+        let pdate = r.properties?.["Date"]?.["date"]?.["start"];
+        if (pdate) {
+            date = moment(pdate).format("YYYY-MM-DD");
+        }
+
+        // title
+        let title = id;
+        let ptitle = r.properties?.["Title"]?.["title"];
+        if (ptitle?.length > 0) {
+            title = ptitle[0]?.["plain_text"];
+        }
+
+        // tags
+        let tags = [];
+        let ptags = r.properties?.["Tags"]?.["multi_select"];
+        for (const t of ptags) {
+            const n = t?.["name"];
+            if (n) {
+                tags.push(n);
+            }
+        }
+
+        // categories
+        let cats = [];
+        let pcats = r.properties?.["Categories"]?.["multi_select"];
+        for (const t of pcats) {
+            const n = t?.["name"];
+            if (n) {
+                cats.push(n);
+            }
+        }
+
+        // frontmatter
+        let fmtags = "";
+        let fmcats = "";
+        if (tags.length > 0) {
+            fmtags += "\ntags:\n";
+            for (const t of tags) {
+                fmtags += "  - " + t + "\n";
+            }
+        }
+        if (cats.length > 0) {
+            fmcats += "\ncategories:\n";
+            for (const t of cats) {
+                fmcats += "  - " + t + "\n";
+            }
+        }
+        const fm = `---
+title: ${title}${fmcats}${fmtags} 
+---
+`;
+
+        const mdblocks = await n2m.pageToMarkdown(id);
+        let md = n2m.toMarkdownString(mdblocks)["parent"];
+        if (md === "") {
+            continue;
+        }
+        md = escapeCodeBlock(md);
+        md = replaceTitleOutsideRawBlocks(md);
+
+        const ftitle = `${date}-${title.replaceAll(" ", "-")}.md`;
+
+        let index = 0;
+        let edited_md = md.replace(
+            /!\[(.*?)\]\((.*?)\)/g,
+            function (match, p1, p2, p3) {
+                const dirname = path.join("assets/img", ftitle);
+                if (!fs.existsSync(dirname)) {
+                    fs.mkdirSync(dirname, { recursive: true });
+                }
+                const filename = path.join(dirname, `${index}.png`);
+
+                axios({
+                    method: "get",
+                    url: p2,
+                    responseType: "stream",
+                })
+                    .then(function (response) {
+                        let file = fs.createWriteStream(`${filename}`);
+                        response.data.pipe(file);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+
+                let res;
+                if (p1 === "") res = "";
+                else res = `_${p1}_`;
+
+                return `![${index++}](/${filename})${res}`;
+            }
+        );
+
+        //writing to file
+        fs.writeFile(path.join(root, ftitle), fm + edited_md, (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+})();
