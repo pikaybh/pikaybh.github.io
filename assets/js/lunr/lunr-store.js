2a0511a0f5561d3a5b4c38a652ffd2ed8e12a8e8
pikaybh <pikaybh@users.noreply.github.com>
2025-02-22 11:16:04 +0000

    jekyll build from Action 7a580ad67f13e03e4407caa0f188169536cd8ec5

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,190 @@
+var store = [{
+        "title": "1000번: A+B",
+        "excerpt":"개요 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 첫째 줄에 A+B를 출력한다. 예제 입력 1 1 2 예제 출력 1 3 내 풀이 C 메모리 시간 코드 길이 1112KB 0ms 186B #include &lt;stdio.h&gt; int...","categories": ["BOJ"],
+        "tags": ["Bronze V","C","Python","Node.js"],
+        "url": "/1000-AB/",
+        "teaser": null
+      },{
+        "title": "1001번: A-B",
+        "excerpt":"개요 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 첫째 줄에 A-B를 출력한다. 예제 입력 1 3 2 예제 출력 1 1 내 풀이 Python 3 메모리 시간 코드 길이 30840KB 68ms 47B a, b...","categories": ["BOJ"],
+        "tags": ["Bronze V","Python"],
+        "url": "/1001-A-B/",
+        "teaser": null
+      },{
+        "title": "1008번: A/B",
+        "excerpt":"개요 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다. 예제 입력 1 1 3 예제 출력 1 0.33333333333333333333333333333333 10-9 이하의 오차를 허용한다는...","categories": ["BOJ"],
+        "tags": ["Bronze V","Python"],
+        "url": "/1008-AB/",
+        "teaser": null
+      },{
+        "title": "1271번: 엄청난 부자2",
+        "excerpt":"개요 갑부 최백준 조교는 동전을 최소로 바꾸는데 성공했으나 김재홍 조교가 그 돈을 발견해서 최백준 조교에게 그 돈을 나누자고 따진다. 그 사실이 전 우주로 알려지자 우주에 있던 많은 생명체들이 자신들에게 돈을 분배해 달라고 당장 달려오기 시작했다. 프로토스 중앙 우주 정부의 정책인, ‘모든 지적 생명체는 동등하다’라는 규칙에 입각해서 돈을 똑같이 분배하고자 한다....","categories": ["BOJ"],
+        "tags": ["Bronze V","Python"],
+        "url": "/1271-2/",
+        "teaser": null
+      },{
+        "title": "1330번: 두 수 비교하기",
+        "excerpt":"개요 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다. 출력 첫째 줄에 다음 세 가지 중 하나를 출력한다. A가 B보다 큰 경우에는 ‘&gt;’를 출력한다. A가 B보다 작은 경우에는 ‘&lt;’를 출력한다. A와 B가 같은 경우에는...","categories": ["BOJ"],
+        "tags": ["Bronze V","Python"],
+        "url": "/1330/",
+        "teaser": null
+      },{
+        "title": "OSCP Cheatsheet",
+        "excerpt":"VI :w !sudo tree % Windows Command Formatting echo \"&lt;COMMAND&gt;\" | iconv -f UTF-8 -t UTF-16LE | base64 -w0 Microsoft Windows dir dir flag* /s /p dir /s /b *.log PHP Webserver sudo php -S 127.0.0.1:80 Ping ping -c 1 &lt;RHOST&gt; ping -n 1 &lt;RHOST&gt; Python Webserver sudo python -m...","categories": ["Cheatsheet"],
+        "tags": ["OSCP"],
+        "url": "/OSCP-Cheatsheet/",
+        "teaser": null
+      },{
+        "title": "Comprehensive Guide to Network Scanning with Nmap",
+        "excerpt":"Nmap (Network Mapper) is an essential tool for network security assessment and vulnerability analysis. This post provides an in-depth guide to Nmap’s key features and applications. For demonstration purposes, we will use 127.0.0.1 (localhost) as an example. 1. Basic Nmap Scan 🔹 Basic Port Scan (1,000 Common Ports) nmap 127.0.0.1 Checks if...","categories": ["Cheatsheet"],
+        "tags": ["Nmap","OSCP"],
+        "url": "/Comprehensive-Guide-to-Network-Scanning-with-Nmap/",
+        "teaser": null
+      },{
+        "title": "Ruby Date Format (strftime)",
+        "excerpt":"Date Formatting Format Description %Y Year with century (e.g. 2015, 1995, etc) %m Month of the year, zero-padded (01..12) %B The full month name (e.g. January) %b The abbreviated month name (e.g. Jan) %d Day of the month, zero-padded (01..31) %j Day of the year (001..366) %C Year / 100...","categories": ["Cheatsheet"],
+        "tags": ["Ruby","OSCP"],
+        "url": "/Ruby-Date-Format-strftime/",
+        "teaser": null
+      },{
+        "title": "Vim Cheatsheet: Essential Commands for Developers",
+        "excerpt":"Vim is a powerful text editor that can significantly enhance productivity when mastered. This cheatsheet covers fundamental to advanced commands, helping both beginners and experienced developers navigate Vim effectively. 1. Introduction Vim is a free and open-source, screen-based text editor that serves as an enhanced version of the original vi...","categories": ["Cheatsheet"],
+        "tags": ["OSCP","Linux","Vim"],
+        "url": "/Vim-Cheatsheet-Essential-Commands-for-Developers/",
+        "teaser": "/assets/images/headers/1_VO98sILElESsQdiq1DLcZQ.png"
+      },{
+        "title": "Solving OverTheWire's Bandit: A Beginner's Journey into Linux CTF",
+        "excerpt":"Introduction OverTheWire’s Bandit wargame is an excellent starting point for anyone looking to improve their Linux command-line skills, especially in the context of cybersecurity and Capture The Flag (CTF) challenges. This blog post outlines my experience solving Bandit, the key lessons I learned, and tips for beginners tackling the challenge....","categories": ["Wargames"],
+        "tags": ["Linux"],
+        "url": "/Solving-OverTheWires-Bandit-A-Beginners-Journey-into-Linux-CTF/",
+        "teaser": "/assets/images/headers/download.png"
+      },{
+        "title": "Understanding TCP Sessions: How Connections Are Established, Maintained, and Terminated",
+        "excerpt":"In the realm of computer networking, the Transmission Control Protocol (TCP) plays a pivotal role in ensuring reliable communication between devices. A fundamental concept within TCP is the TCP session, which facilitates structured and dependable data exchange. What is a TCP Session? A TCP session, also known as a TCP...","categories": ["Notes"],
+        "tags": ["TCP/UDP","IPv4","IPv6"],
+        "url": "/Understanding-TCP-Sessions-How-Connections-Are-Established-Maintained-and-Terminated/",
+        "teaser": "/assets/images/headers/DALL%C2%B7E%202025-02-10%2001.47.53%20-%20A%20wide-angle%20realistic%20illustration%20of%20two%20hands%20shaking%20in%20a%20professional%20handshake.%20The%20handshake%20symbolizes%20agreement,%20connection,%20and%20trust.%20The%20b.webp"
+      },{
+        "title": "Why Use Axios Over Fetch in JavaScript?",
+        "excerpt":"When working with HTTP requests in JavaScript, you might wonder why many developers prefer axios over the built-in fetch API. While fetch is natively available and works well for basic requests, axios offers several advantages that make it a more convenient choice in real-world applications. In this article, we’ll compare...","categories": ["Blog"],
+        "tags": ["Javascript","Node.js"],
+        "url": "/Why-Use-Axios-Over-Fetch-in-JavaScript/",
+        "teaser": "/assets/images/headers/img.jpeg"
+      },{
+        "title": "A Study on the Effectiveness Analysis and Improvement of On-Site Operability of Hazard and Risk Prevention Plans in the Construction Industry",
+        "excerpt":"with KOSHA(Korea Occupational Safety and Health Agency) 2024.05 ~ 2024.11 In order for the hazard and risk prevention plan system to ensure on-site operability and effectiveness in preventing accidents at construction sites, the operation system of the plan must be useful without imposing an excessive burden in practice, from the...","categories": ["Portfolio"],
+        "tags": [],
+        "url": "/projects/A-Study-on-the-Effectiveness-Analysis-and-Improvement-of-On-Site-Operability-of-Hazard-and-Risk-Prevention-Plans-in-the-Construction-Industry/",
+        "teaser": "/assets/images/headers/RaWMwfQQvUkWLvgQfNIVTccL0ZeUPXnIQ79FzEXH2DkYlXNwjHlbVX4ekYUx12hjCKBN8O9tOQbSBxGrQAeR5w.svg"
+      },{
+        "title": "Development of a Large Language Model-Based Risk Assessment System for Preventing Construction Site Accidents",
+        "excerpt":"with Ministry of SMEs and Startups 2024.10 ~ 2025.09 This study aims to maximize the efficiency and accuracy of safety management and risk assessment in the construction industry by developing a safety service based on a large-scale language model. The developed service will predict and assess potential risk factors that...","categories": ["Portfolio"],
+        "tags": [],
+        "url": "/projects/Development-of-a-Large-Language-Model-Based-Risk-Assessment-System-for-Preventing-Construction-Site-Accidents/",
+        "teaser": "/assets/images/headers/4916_7556_3548.jpg"
+      },{
+    "title": "1000번: A+B",
+    "excerpt":"개요 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 첫째 줄에 A+B를 출력한다. 예제 입력 1 1 2 예제 출력 1 3 내 풀이 C 메모리 시간 코드 길이 1112KB 0ms 186B #include &lt;stdio.h&gt; int...","url": "https://pikaybh.github.io/_posts/2022-12-20-1000-AB/"
+  },{
+    "title": "1001번: A-B",
+    "excerpt":"개요 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 첫째 줄에 A-B를 출력한다. 예제 입력 1 3 2 예제 출력 1 1 내 풀이 Python 3 메모리 시간 코드 길이 30840KB 68ms 47B a, b...","url": "https://pikaybh.github.io/_posts/2022-12-20-1001-A-B/"
+  },{
+    "title": "1008번: A/B",
+    "excerpt":"개요 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다. 예제 입력 1 1 3 예제 출력 1 0.33333333333333333333333333333333 10-9 이하의 오차를 허용한다는...","url": "https://pikaybh.github.io/_posts/2022-12-20-1008-AB/"
+  },{
+    "title": "1271번: 엄청난 부자2",
+    "excerpt":"개요 갑부 최백준 조교는 동전을 최소로 바꾸는데 성공했으나 김재홍 조교가 그 돈을 발견해서 최백준 조교에게 그 돈을 나누자고 따진다. 그 사실이 전 우주로 알려지자 우주에 있던 많은 생명체들이 자신들에게 돈을 분배해 달라고 당장 달려오기 시작했다. 프로토스 중앙 우주 정부의 정책인, ‘모든 지적 생명체는 동등하다’라는 규칙에 입각해서 돈을 똑같이 분배하고자 한다....","url": "https://pikaybh.github.io/_posts/2022-12-20-1271--2/"
+  },{
+    "title": "1330번: 두 수 비교하기",
+    "excerpt":"개요 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다. 출력 첫째 줄에 다음 세 가지 중 하나를 출력한다. A가 B보다 큰 경우에는 ‘&gt;’를 출력한다. A가 B보다 작은 경우에는 ‘&lt;’를 출력한다. A와 B가 같은 경우에는...","url": "https://pikaybh.github.io/_posts/2022-12-20-1330---/"
+  },{
+    "title": "OSCP Cheatsheet",
+    "excerpt":"VI :w !sudo tree % Windows Command Formatting echo \"&lt;COMMAND&gt;\" | iconv -f UTF-8 -t UTF-16LE | base64 -w0 Microsoft Windows dir dir flag* /s /p dir /s /b *.log PHP Webserver sudo php -S 127.0.0.1:80 Ping ping -c 1 &lt;RHOST&gt; ping -n 1 &lt;RHOST&gt; Python Webserver sudo python -m...","url": "https://pikaybh.github.io/_posts/2025-01-28-OSCP-Cheatsheet/"
+  },{
+    "title": "Comprehensive Guide to Network Scanning with Nmap",
+    "excerpt":"Nmap (Network Mapper) is an essential tool for network security assessment and vulnerability analysis. This post provides an in-depth guide to Nmap’s key features and applications. For demonstration purposes, we will use 127.0.0.1 (localhost) as an example. 1. Basic Nmap Scan 🔹 Basic Port Scan (1,000 Common Ports) nmap 127.0.0.1 Checks if...","url": "https://pikaybh.github.io/_posts/2025-01-30-Comprehensive-Guide-to-Network-Scanning-with-Nmap/"
+  },{
+    "title": "Ruby Date Format (strftime)",
+    "excerpt":"Date Formatting Format Description %Y Year with century (e.g. 2015, 1995, etc) %m Month of the year, zero-padded (01..12) %B The full month name (e.g. January) %b The abbreviated month name (e.g. Jan) %d Day of the month, zero-padded (01..31) %j Day of the year (001..366) %C Year / 100...","url": "https://pikaybh.github.io/_posts/2025-02-01-Ruby-Date-Format-strftime/"
+  },{
+    "title": "Vim Cheatsheet: Essential Commands for Developers",
+    "excerpt":"Vim is a powerful text editor that can significantly enhance productivity when mastered. This cheatsheet covers fundamental to advanced commands, helping both beginners and experienced developers navigate Vim effectively. 1. Introduction Vim is a free and open-source, screen-based text editor that serves as an enhanced version of the original vi...","url": "https://pikaybh.github.io/_posts/2025-02-02-Vim-Cheatsheet-Essential-Commands-for-Developers/"
+  },{
+    "title": "Solving OverTheWire's Bandit: A Beginner's Journey into Linux CTF",
+    "excerpt":"Introduction OverTheWire’s Bandit wargame is an excellent starting point for anyone looking to improve their Linux command-line skills, especially in the context of cybersecurity and Capture The Flag (CTF) challenges. This blog post outlines my experience solving Bandit, the key lessons I learned, and tips for beginners tackling the challenge....","url": "https://pikaybh.github.io/_posts/2025-02-08-Solving-OverTheWires-Bandit-A-Beginners-Journey-into-Linux-CTF/"
+  },{
+    "title": "Understanding TCP Sessions: How Connections Are Established, Maintained, and Terminated",
+    "excerpt":"In the realm of computer networking, the Transmission Control Protocol (TCP) plays a pivotal role in ensuring reliable communication between devices. A fundamental concept within TCP is the TCP session, which facilitates structured and dependable data exchange. What is a TCP Session? A TCP session, also known as a TCP...","url": "https://pikaybh.github.io/_posts/2025-02-09-Understanding-TCP-Sessions-How-Connections-Are-Established-Maintained-and-Terminated/"
+  },{
+    "title": "Why Use Axios Over Fetch in JavaScript?",
+    "excerpt":"When working with HTTP requests in JavaScript, you might wonder why many developers prefer axios over the built-in fetch API. While fetch is natively available and works well for basic requests, axios offers several advantages that make it a more convenient choice in real-world applications. In this article, we’ll compare...","url": "https://pikaybh.github.io/_posts/2025-02-21-Why-Use-Axios-Over-Fetch-in-JavaScript/"
+  },{
+    "title": "Page Not Found",
+    "excerpt":"Sorry, but the page you were trying to view does not exist.      ","url": "https://pikaybh.github.io/404.html"
+  },{
+    "title": "About Me",
+    "excerpt":"I am a Ph.D. researcher at Seoul National University (SNU). I received my bachelor’s degree from Dankook University in 2023 and my master’s degree from SNU in 2025, respectively. I am currently located in Seoul, Korea, and work as a researcher in Seoul National University Construction Engineering &amp; Management Lab....","url": "https://pikaybh.github.io/about/"
+  },{
+    "title": "About Me (Academic Version)",
+    "excerpt":"I am a Ph.D. researcher at Seoul National University (SNU). I received my bachelor’s degree from Dankook University in 2023 and my master’s degree from SNU in 2025, respectively. I am currently located in Seoul, Korea, and work as a researcher in Seoul National University Construction Engineering &amp; Management Lab....","url": "https://pikaybh.github.io/academy/"
+  },{
+    "title": "Archive Layout with Content",
+    "excerpt":"A variety of common markup showing how the theme styles them. Header one Header two Header three Header four Header five Header six Blockquotes Single line blockquote: Stay hungry. Stay foolish. Multi line blockquote with a cite reference: People think focus means saying yes to the thing you’ve got to...","url": "https://pikaybh.github.io/archive-layout-with-content/"
+  },{
+    "title": "Articles",
+    "excerpt":"             ","url": "https://pikaybh.github.io/articles/"
+  },{
+    "title": "Posts by Category",
+    "excerpt":" ","url": "https://pikaybh.github.io/categories/"
+  },{
+    "title": "Posts by Collection",
+    "excerpt":"projects A Study on the Effectiveness Analysis and Improvement of On-Site Operability of Hazard and Risk Prevention Plans in the Construction Industry less than 1 minute read with KOSHA (2024.05 ~ 2024.11) Development of a Large Language Model-Based Risk Assessment System for Preventing Construction Site Accidents less than 1 minute...","url": "https://pikaybh.github.io/collection-archive/"
+  },{
+    "title": null,
+    "excerpt":" ","url": "https://pikaybh.github.io/home/"
+  },{
+    "title": null,
+    "excerpt":"     ","url": "https://pikaybh.github.io/"
+  },{
+    "title": null,
+    "excerpt":"var idx = lunr(function () { this.field('title') this.field('excerpt') this.field('categories') this.field('tags') this.ref('id') this.pipeline.remove(lunr.trimmer) for (var item in store) { this.add({ title: store[item].title, excerpt: store[item].excerpt, categories: store[item].categories, tags: store[item].tags, id: item }) } }); $(document).ready(function() { $('input#search').on('keyup', function () { var resultdiv = $('#results'); var query = $(this).val().toLowerCase(); var result = idx.query(function...","url": "https://pikaybh.github.io/assets/js/lunr/lunr-en.js"
+  },{
+    "title": null,
+    "excerpt":"step1list = new Array(); step1list[\"ΦΑΓΙΑ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΟΥ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΩΝ\"] = \"ΦΑ\"; step1list[\"ΣΚΑΓΙΑ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΟΥ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΩΝ\"] = \"ΣΚΑ\"; step1list[\"ΟΛΟΓΙΟΥ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΑ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΩΝ\"] = \"ΟΛΟ\"; step1list[\"ΣΟΓΙΟΥ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΑ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΩΝ\"] = \"ΣΟ\"; step1list[\"ΤΑΤΟΓΙΑ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΟΥ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΩΝ\"] = \"ΤΑΤΟ\"; step1list[\"ΚΡΕΑΣ\"]...","url": "https://pikaybh.github.io/assets/js/lunr/lunr-gr.js"
+  },{
+    "title": null,
+    "excerpt":"var store = [ {%- for c in site.collections -%} {%- if forloop.last -%} {%- assign l = true -%} {%- endif -%} {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%} {%- for doc in docs -%} {%- if doc.header.teaser -%} {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture...","url": "https://pikaybh.github.io/assets/js/lunr/lunr-store.js"
+  },{
+    "title": "Posts",
+    "excerpt":"","url": "https://pikaybh.github.io/posts/"
+  },{
+    "title": "Projects",
+    "excerpt":"   {% for post in site.projects %}     {% include archive-single.html layout=\"half\" type=\"grid\" %}   {% endfor %} ","url": "https://pikaybh.github.io/projects/"
+  },{
+    "title": "Publications",
+    "excerpt":"     {% bibliography %}   Conferences       {% bibliography -f conferences %}    {{ site.data.ui-text[site.locale].back_to_top | default: 'Back to Top' }} &uarr; ","url": "https://pikaybh.github.io/publications/"
+  },{
+    "title": "Sitemap",
+    "excerpt":"A list of all the posts and pages found on the site. For you robots out there is an [XML version]({{ \"sitemap.xml\" | relative_url }}) available for digesting as well. Pages {% for post in site.pages %} {% unless post.sitemap == false %} {% include archive-single.html %} {% endunless %}...","url": "https://pikaybh.github.io/sitemap/"
+  },{
+    "title": "Posts by Tags",
+    "excerpt":"","url": "https://pikaybh.github.io/tags/"
+  },{
+    "title": "Terms and Privacy Policy",
+    "excerpt":"{% include base_path %} {% include toc %} ## Privacy Policy The privacy of my visitors is extremely important. This Privacy Policy outlines the types of personal information that is received and collected and how it is used. First and foremost, I will never share your email address or any...","url": "https://pikaybh.github.io/terms/"
+  },{
+    "title": "Posts by Year",
+    "excerpt":"","url": "https://pikaybh.github.io/year-archive/"
+  },{
+    "title": null,
+    "excerpt":"     ","url": "https://pikaybh.github.io/"
+  },{
+    "title": null,
+    "excerpt":"     ","url": "https://pikaybh.github.io/"
+  },{
+    "title": null,
+    "excerpt":"{% if page.xsl %} {% endif %} {% assign collections = site.collections | where_exp:'collection','collection.output != false' %}{% for collection in collections %}{% assign docs = collection.docs | where_exp:'doc','doc.sitemap != false' %}{% for doc in docs %} {{ doc.url | replace:'/index.html','/' | absolute_url | xml_escape }} {% if doc.last_modified_at or doc.date...","url": "https://pikaybh.github.io/sitemap.xml"
+  },{
+    "title": null,
+    "excerpt":"{% if page.xsl %}{% endif %}Jekyll{{ site.time | date_to_xmlschema }}{{ page.url | absolute_url | xml_escape }}{% assign title = site.title | default: site.name %}{% if page.collection != \"posts\" %}{% assign collection = page.collection | capitalize %}{% assign title = title | append: \" | \" | append: collection %}{% endif...","url": "https://pikaybh.github.io/feed.xml"
+  }]
